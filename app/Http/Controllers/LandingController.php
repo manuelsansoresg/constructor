d5e300a84353b0e6314bad707d44bde531fd6704
manuelsansores <manuelsansoresg@gmail.com>
2023-01-31 20:00:00 -0600

    prueba

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -17,7 +17,10 @@ class LandingController extends Controller
     {
         
         $page           = ($request->segment(1) === null) ? '/' : $request->segment(1);
-        $page_actual    = Builder::where('slug', $page)->first();
+        $setting = Setting::get();
+        $page_actual    = Builder::where('slug', $page)
+                            ->where('setting_id', $setting->id)
+                            ->first();
         $my_widgets     = WidgetBuilder::getMyWidgets($page_actual->id);
         $my_setting     = Setting::get();
         $pages          = Builder::where('slug', '!=', '/')->get();
